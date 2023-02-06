0e514c7ba9bd6ea847a221ee6706f94c7de5bfc9
hudua <40040433+hudua@users.noreply.github.com>
2023-02-06 13:14:51 -0500

    Update databricks-model-training-automation.py

--- pipelines/databricks-model-training-automation.py
+++ pipelines/databricks-model-training-automation.py
@@ -1,28 +1,38 @@
 import sys
 from databricks_api import DatabricksAPI
-import time
+import time, json
 
-tokenuser = sys.argv[1]
-build_id = sys.argv[2]
+env = sys.argv[1]
+token = sys.argv[2]
+build_id = sys.argv[3]
+print(env)
+
+configs = json.load(open('configs/settings.json',))
+
+host = configs[env]['host']['value']
+amlname = configs[env]['amlname']['value']
+repo_id = configs[env]['repo_id']['value']
+cluster_id = configs[env]['cluster_id']['value']
 
 db = DatabricksAPI(
-    host="https://adb-312205465995014.14.azuredatabricks.net/",
-    token=tokenuser
+    host=host,
+    token=token
 )
 
 db.repos.update_repo(
-    id = 2580158515741092,
+    id = repo_id,
     branch='main'
 )
 
 job_id = db.jobs.create_job(
     name="ml-ops-model-training-{}".format(build_id),
-    existing_cluster_id="0114-031312-mfx6svv5",
-    notebook_task={"notebook_path":"/Repos/msft-hudua-data-ml-ops/repo/ml/main"}
+    existing_cluster_id=cluster_id,
+    notebook_task={"notebook_path":"/Repos/EnterpriseMLHub/MLHub/Model/modeling"}
 )['job_id']
 
 run_id = db.jobs.run_now(
-    job_id=job_id
+    job_id=job_id,
+    notebook_params = {"amlname": amlname, "build_id": build_id}
 )['run_id']
 
 N = 30
@@ -34,9 +44,10 @@ for i in range(N):
     if status == 'SUCCESS':
         print('Job is successful')
         break
+    elif status == 'FAILED':
+        raise Exception('Job failed')
     else:
         print('Job is not successful yet, waiting 1 minute')
         time.sleep(60)
 if i == N - 1:
     raise Exception('job timed out after 30 mins. Please check Databricks job')
-        
